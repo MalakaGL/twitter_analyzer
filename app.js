c407f685e3c8ee164eaa3c626fa2fb0a05b25690
MALAKAGL <glmalaka@gmail.com>
2015-01-19 15:40:50 +0530

    ENOENT error.

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html', {root: 'public/html' })
+    //res.sendFile('index.html', {root: 'public/html' })
+	response.send('Hello World!');
 });
 
 app.get('/graph', function(req, res) {
