b2b3e831dc9acae0fe133c5ba549d45261b4b949
MALAKAGL <glmalaka@gmail.com>
2015-01-19 15:37:52 +0530

    ENOENT error.

--- app.js
+++ app.js
@@ -8,31 +8,25 @@ var port = 8888
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-	response.send('Hello World!');
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: 'public/html' })
+});
+
+app.get('/graph', function(req, res) {
+	db.data('_id', 1, 1000, function(err, data){
+		res.json(data);
+	})
+});
+
+app.get('/getData/', function(req, res) {
+	db.getDataRange(req['query'], function(err, data){
+		res.json(data);
+	})
 });
 
 app.listen(app.get('port'), function() {
 	console.log("Node app is running at localhost:" + app.get('port'));
 });
-
-//
-//app.get('/', function(req, res) {
-//    res.sendFile('index.html', {root: 'public/html' })
-//});
-//
-//app.get('/graph', function(req, res) {
-//	db.data('_id', 1, 1000, function(err, data){
-//		res.json(data);
-//	})
-//});
-//
-//app.get('/getData/', function(req, res) {
-//	db.getDataRange(req['query'], function(err, data){
-//		res.json(data);
-//	})
-//});
-
 
 //app.get('/latest', function(req, res) {
 //	db.data('_id', -1, 10, function(err, data){
